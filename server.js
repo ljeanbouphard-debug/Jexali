047cf098377f063e91239a3b10b799ad21722f0e
ljeanbouphard-debug <ljeanbouphard@gmail.com>
2026-09-11 04:53:56 -0400

    Fix syntax error on line 14

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.use(express.json());
 app.use(express.static(__dirname));
 app.get('/api/key-length', (req, res) => {
  res.json({ length:
-  process.env.STRIPE_SECRET_KEY? });
+  process.env.STRIPE_SECRET_KEY ?
+  process.env.STRIPE_SECRET_KEY.length :
+  O });
 });
 app.get('/api/stripe-test', async (req,res) => {
  try { const account = await stripe.accounts.retrieve();
