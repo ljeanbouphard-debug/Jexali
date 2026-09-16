dcc2fb99af94926a51816d0271fd9583d7b947a4
ljeanbouphard-debug <ljeanbouphard@gmail.com>
2026-09-16 04:32:21 -0400

    Fix checkout product validation block

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ cart.length===0)
  const dbProducts = (await db.query('SELECT * FROM products WHERE id = ANY($1::int[])',[productIds])).rows;
 console.log("DB PRODUCTS FOUND:",dbProducts.length, dbProducts);
 console.log("CHECKOUT LENGTHS:", dbProducts.length, productIds.length);
- if (dbProducts.lentgh !== productIds.length) return res.status(400).json({error:'Invalid product in cart'});
+ if (dbProducts.length !== productIds.length) { return res.status(400).json({eror:'Invalid product in cart'});}
 const sellerIds = dbProducts.map(product => product.seller_id);
  const allSameSellerId = sellerIds.every(id => id === sellerIds[0]);
 if (!allSameSellerId || !sellerIds[0]) return res.status(400).json({error:'Products must belong to one valid seller'});
