9b941778e31641037e5fea3dab468988bbafa049
ljeanbouphard-debug <ljeanbouphard@gmail.com>
2026-09-16 00:47:06 -0400

    Fix app.js syntax error

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function renderShop(){
 document.getElementById("searchInput").addEventListener("input",renderShop);
 
 function addToCart(id){
-  cont p=allProducts().find(x=>String(x.id)===String(id));
+  const p=allProducts().find(x=>String(x.id)===String(id));
   if(!p) return;
   cart.push({...p,cartId:Date.now()+Math.random()});
   localStorage.setItem("jexaliCart",JSON.stringify(cart));
