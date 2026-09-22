bda7dfd803cc30c44fcb951b40407ff8768309c0
ljeanbouphard-debug <ljeanbouphard@gmail.com>
2026-09-22 02:14:24 -0400

    Restore working server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const cors = require('cors')
 const express = require('express');
 const session = require("express-session");
-const pgSession = require("connect-pg-simple")(session);
+
 const stripe = require('stripe') (process.env.STRIPE_SECRET_KEY);
 const stripeTest = require('stripe') (process.env.STRIPE_TEST_SECRET_KEY);
 
@@ -12,7 +12,7 @@ const app = express();
 app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json());
-app.use(session({store: newpgSession({ pool: db, createTableIfMissing: true }), 
+app.use(session({ 
   secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false, cookie: {httpOnly: true, secure: process.env.NODE_ENV === "production", sameSite: "lax", maxeAge: 30 * 24 * 60 * 60 * 1000}}));
 app.use(express.static(__dirname));
 app.get('/api/key-length', (req, res) => {
